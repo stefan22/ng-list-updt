502a96a2eadf47491eddec18dbaa3f790422b98e
slissia <slissia@gvcgroup.com>
2018-07-15 23:27:49 +0100

    ..and register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onRegister() {
+    alert('please don\'t do that again');
   }
-
 }
