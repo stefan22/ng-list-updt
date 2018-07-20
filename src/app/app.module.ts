c3c2f07d648b8ed8247333f0076645b6bf6a92a0
slissia <slissia@gvcgroup.com>
2018-07-20 18:42:25 +0100

    guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NavComponent } from "./nav/nav.component";
+import { NavComponent } from './nav/nav.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from "./register/register.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 import { LandingComponent } from './landing/landing.component';
-import { NotFoundComponent } from "./404/404.component";
+import { NotFoundComponent } from './404/404.component';
 
 import { ProductsComponent } from './products/products.component';
 import { DetailsComponent } from './details/details.component';
@@ -20,13 +20,16 @@ import { HowToComponent } from './how-to/how-to.component';
 
 import { ProductsService } from './services/products.service';
 import { AdminComponent } from './admin/admin.component';
+import { LoggedInGuard } from './services/logged-in.guard';
+import { UsersComponent } from './users/users.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { PermissionsComponent } from './permissions/permissions.component';
 
 
 
 const appRoutes: Routes = [
-  
   {
-    path: '', 
+    path: '',
     component: DashboardComponent
   },
   {
@@ -40,13 +43,12 @@ const appRoutes: Routes = [
   {
     path: 'password-reset',
     component: PasswordResetComponent
-    
-  },{
+  },
+  {
     path: 'password-reset',
     component: PasswordResetComponent
-    
   },
-  
+
   {
     path: 'register',
     component: RegisterComponent
@@ -66,10 +68,23 @@ const appRoutes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
-    canActivate: [],
-    children: []
+    canActivate: [LoggedInGuard],
+    children: [
+      {
+        path: '', component: UsersComponent
+      },
+      {
+        path: 'user-list', component: UsersComponent
+      },
+      {
+        path: 'add-user', component: AddUserComponent
+      },
+      {
+        path: 'permissions', component: PermissionsComponent
+      }
+    ]
   },
-  
+
   {
     path: '**',
     component: NotFoundComponent
@@ -90,16 +105,14 @@ const appRoutes: Routes = [
     ProductsComponent,
     DetailsComponent,
     HowToComponent,
-    AdminComponent
-   
+    AdminComponent,
+    UsersComponent,
+    AddUserComponent,
+    PermissionsComponent
   ],
-  imports: [
-    BrowserModule, 
-    HttpClientModule, 
-    RouterModule.forRoot(appRoutes)
-  ],
-  
-  providers: [ProductsService],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+
+  providers: [ProductsService, LoggedInGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
