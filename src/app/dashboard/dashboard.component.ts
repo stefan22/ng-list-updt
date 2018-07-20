0adbb98abbd383ae6d30cb325b021ddf06691fe3
slissia <slissia@gvcgroup.com>
2018-07-20 12:57:56 +0100

    last update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-dashboard",
-  templateUrl: "./dashboard.component.html",
-  styleUrls: ["./dashboard.component.css"]
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  isHomePage = "jumbo hm";
+  isHomePage = 'jumbo hm';
+  bookTitle: string = 'Internet of Things';
   upp: boolean = false;
   constructor() {}
 
